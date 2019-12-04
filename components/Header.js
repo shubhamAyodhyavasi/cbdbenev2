ae29bcab1c5026c1d6c4374993986f7c8b601175
thirdessential <thirdessential@gmail.com>
2019-12-04 17:44:13 +0530

    Nav position - Anas

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,8 @@
-import {useState} from 'react'
+
+import React, { useState, useRef, useMemo, Fragment } from 'react'
 import Logo from "./Logo";
 import Nav from "./nav";
+import { useScrollPosition } from "../services/helpers/scroll"
 import { Affix } from 'antd'
 import classNames from 'classnames'
 import mainMenus from '../constants/mainMenus'
@@ -8,8 +10,24 @@ import rightMenus from "../constants/rightMenus";
 const Header = ({bg, theme, versions}) => {
     const versionClass = versions.map(el => (`c-header--${el}`)).join(" ")
     const [isFixed, setIsFixed] = useState(false)
+   const [hideOnScroll, setHideOnScroll] = useState(true)
+
+  useScrollPosition(
+    ({ prevPos, currPos }) => {
+        console.log({"prevPos": prevPos.y})
+        console.log({"currPos":currPos.y})
+      const isShow = currPos.y > prevPos.y
+      if (isShow !== hideOnScroll) setHideOnScroll(isShow)
+    },
+    [hideOnScroll],
+    false,
+    false,
+    300
+  )
+
+    
     return (
-        <Affix className="c-header__affix" offsetTop={0} onChange={e => setIsFixed(e)}>
+        // <Affix className="c-header__affix"  onChange={e => setIsFixed(e)}>
             <header className={classNames(" c-header", {
                 "c-header--light": true,
                 "c-header--fixed": false,
@@ -17,6 +35,7 @@ const Header = ({bg, theme, versions}) => {
                 ["c-header--"+theme]: theme,
                 [versionClass]: versions
             })}>
+                {console.log(hideOnScroll)}
                 {/* <nav className="c-header__nav nav nav--main">
                 left nav
                 </nav> */}
@@ -24,7 +43,7 @@ const Header = ({bg, theme, versions}) => {
                 <Logo />
                 <Nav parent="c-header" isRight={true} items={rightMenus} />
             </header>
-        </Affix>
+        //  </Affix>
     )
 }
 
