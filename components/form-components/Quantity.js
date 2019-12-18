437b1b36104138857de7147e155cbe663b735100
shubhamAyodhyavasi <shubhamAyodhyavasi@users.noreply.github.com>
2019-12-18 07:22:19 +0530

    quantity update fix

--- components/form-components/Quantity.js
+++ components/form-components/Quantity.js
@@ -1,31 +1,77 @@
-import {useState} from 'react'
+// import {useState, useEffect} from 'react'
 import classNames from 'classnames'
-const Quantity = ({min, max, value, onChange, parentClass}) => {
-    const parent = `${parentClass}__quantity`
-    const [inputValue, setInputValue] = useState(value || 1)
-    const onInputChange = (e)=> {
-        if(!(e < min || e > max)){
-            setInputValue(e)
-            if(typeof onChange === "function") onChange(e)
+
+class Quantity extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            inputValue : props.value || 1
         }
     }
-    return (
-        <div className={classNames("c-quantity", {
-            [parent]: parentClass
-        })}>
-            <div onClick={()=> {
-                onInputChange(inputValue - 1)
-            }} className="c-quantity__btn c-quantity__btn--minus">-</div>
-            {/* <input className="c-quantity__input" type="number" onChange={onInputChange} min={min} max={max} value={inputValue} /> */}
-            <span className="c-quantity__input">
-                {inputValue}
-            </span>
-            <div onClick={()=> {
-                onInputChange(inputValue + 1)
-            }} className="c-quantity__btn c-quantity__btn--plus">+</div>
-        </div>
-    )
+    componentDidUpdate(prevProps,){
+        if(prevProps.value !== this.props.value){
+            this.setState({
+                inputValue: this.props.value
+            })
+        }
+    }
+    render(){
+        const {min, max, value, onChange, parentClass} = this.props
+        const {inputValue} = this.state
+        const parent = `${parentClass}__quantity`
+        // const [inputValue, setInputValue] = useState(value || 1)
+        const onInputChange = (e)=> {
+            if(!(e < min || e > max)){
+                this.setState({
+                    inputValue : e
+                })
+                if(typeof onChange === "function") onChange(e)
+            }
+        }
+        return (
+            <div className={classNames("c-quantity", {
+                [parent]: parentClass
+            })}>
+                <div onClick={()=> {
+                    onInputChange(inputValue - 1)
+                }} className="c-quantity__btn c-quantity__btn--minus">-</div>
+                {/* <input className="c-quantity__input" type="number" onChange={onInputChange} min={min} max={max} value={inputValue} /> */}
+                <span className="c-quantity__input">
+                    {inputValue}
+                </span>
+                <div onClick={()=> {
+                    onInputChange(inputValue + 1)
+                }} className="c-quantity__btn c-quantity__btn--plus">+</div>
+            </div>
+        )
+    }
 }
+// const Quantity = () => {
+//     const parent = `${parentClass}__quantity`
+//     const [inputValue, setInputValue] = useState(value || 1)
+//     const onInputChange = (e)=> {
+//         if(!(e < min || e > max)){
+//             setInputValue(e)
+//             if(typeof onChange === "function") onChange(e)
+//         }
+//     }
+//     return (
+//         <div className={classNames("c-quantity", {
+//             [parent]: parentClass
+//         })}>
+//             <div onClick={()=> {
+//                 onInputChange(inputValue - 1)
+//             }} className="c-quantity__btn c-quantity__btn--minus">-</div>
+//             {/* <input className="c-quantity__input" type="number" onChange={onInputChange} min={min} max={max} value={inputValue} /> */}
+//             <span className="c-quantity__input">
+//                 {inputValue}
+//             </span>
+//             <div onClick={()=> {
+//                 onInputChange(inputValue + 1)
+//             }} className="c-quantity__btn c-quantity__btn--plus">+</div>
+//         </div>
+//     )
+// }
 Quantity.defaultProps = {
     inputId: `${Math.random() * new Date().getTime()}`
 }
