1b4f00c8f51586bc4f50ea9bd4779e216a4c29c8
Dev-Aman7 <a8285443826@gmail.com>
2020-06-02 23:58:08 +0530

    bug fix

--- constants/projectSettings.js
+++ constants/projectSettings.js
@@ -118,6 +118,7 @@ export const customRates = [
 	},
 ];
 export const topBarText = "Free shipping on all orders above $75."; // make it empty string ("") to hide tha bar
+
 export default {
 	projectName,
 	baseUrl,
